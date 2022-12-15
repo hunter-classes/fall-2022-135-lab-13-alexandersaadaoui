1ead9d394c62b1b393ca448e230d51c595637d7a
alexandersaadaoui <105090016+alexandersaadaoui@users.noreply.github.com>
2022-12-15 00:07:11 -0500

    slight fix

--- tests.cpp
+++ tests.cpp
@@ -6,8 +6,8 @@
 TEST_CASE("A") {
     CHECK(printRange(-2,10) == "-2 -1 0 1 2 3 4 5 6 7 8 9 10");
     CHECK(printRange(5,1) == "");
-	CHECK(printRange(0,3) == "0 1 2 3");
-	CHECK(printRange(-5,1) == "-5 -4 -3 -2 -1 0 1");
+    CHECK(printRange(0,3) == "0 1 2 3");
+    CHECK(printRange(-5,1) == "-5 -4 -3 -2 -1 0 1");
 }
 
 TEST_CASE("B") {
